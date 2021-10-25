ab50792cd42cd269a553012dbaf73878fce9e23f
wulymammoth <861147+wulymammoth@users.noreply.github.com>
2021-10-25 11:51:05 -0400

    UPDATE POE1

--- contracts/ProofOfExistence1.sol
+++ contracts/ProofOfExistence1.sol
@@ -2,6 +2,14 @@
 pragma solidity >=0.4.22 <0.9.0;
 
 contract ProofOfExistence1 {
-  constructor() public {
+  // state
+  bytes32 public proof;
+
+  function notarize(string memory document) public {
+    proof = proofFor(document);
+  }
+
+  function proofFor(string memory document) public pure returns (bytes32) {
+    return sha256(abi.encodePacked(document));
   }
 }
